e4080d492afa872a075c041f289355f4a71e13dd
CORP\dambers <daimen.ambers@capgemini.com>
2019-02-21 15:08:16 -0500

    Changes from Pi

--- mqttPublish.py
+++ mqttPublish.py
@@ -2,13 +2,25 @@
 from math import *
 from random import randint
 import paho.mqtt.publish as publish
+import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
 import time
 from datetime import datetime
+#import socket
+
+client = mqtt.Client()
 
 #Defining Constants
 rand = 0
-freq = 4
+res = 1
+V0 = 12
+I0 = 20
+freq = 0
+Irms = 0
+
+#Get ip address
+hostname = socket.gethostname()
+ipAddr = socket.gethostbyname(hostname)
 
 def DCVoltage(): #Voltage coming from the PLC
     Vc = GPIO.input(11)
@@ -18,16 +30,15 @@ def DCVoltage(): #Voltage coming from the PLC
         status = "Off"
     return status
 
-def voltage(freq): #3 phase voltage from
-    V0 = 5 #volts
+def voltage(freq, res): #3 phase voltage from
+    resConst = 5
     d = datetime.now()
     uSec = d.microsecond
     mSec = uSec / 1000.0
-    Vrms = V0/(sqrt(2))
-    fs = 1
-    V1 = V0*sin(2*pi*freq*mSec/fs)
-    V2 = V0*sin(2*pi*freq*mSec/fs + pi*2/3)
-    V3 = V0*sin(2*pi*freq*mSec/fs + pi*4/3)
+    Vrms = (I0*(resConst*res))/(sqrt(2))
+    V1 = (I0*(resConst*res))*sin(2*pi*freq*mSec)
+    V2 = (I0*(resConst*res))*sin(2*pi*freq*mSec + pi*2/3)
+    V3 = (I0*(resConst*res))*sin(2*pi*freq*mSec + pi*4/3)
     msgs=[{ #Message that will publish the three voltage values on the same graph
     "topic":"Motor/voltage1"
     ,"payload": V1}
@@ -35,17 +46,17 @@ def voltage(freq): #3 phase voltage from
     ,("Motor/voltage3", V3)]
     return msgs
 
-def current(freq): #input the frequency from frequency function
-    I0 = 20 #amps
+def current(freq, res): #input the frequency from frequency function
+    resConst = 5
     d = datetime.now()
     uSec = d.microsecond
     secs = d.second
     mSec = uSec / 1000.0
-    fs = 1
-    Irms = I0/(sqrt(2))
-    I1 = I0*sin(2*pi*freq*mSec/fs)
-    I2 = I0*sin(2*pi*freq*mSec/fs + pi*2/3)
-    I3 = I0*sin(2*pi*freq*mSec/fs + pi*4/3)
+    global Irms
+    Irms = (V0/(resConst*res))/(sqrt(2))
+    I1 = (V0/(resConst*res))*sin(2*pi*freq*mSec)
+    I2 = (V0/(resConst*res))*sin(2*pi*freq*mSec + pi*2/3)
+    I3 = (V0/(resConst*res))*sin(2*pi*freq*mSec + pi*4/3)
     msgs=[{ #Message that will publish the three voltage values on the same graph
         "topic":"Motor/current1"
         ,"payload": I1}
@@ -54,38 +65,44 @@ def current(freq): #input the frequency from frequency function
     return msgs
 
 
-def temperature(): #constant temperature
-    rand = randint(1,50)
-    temp = 50 + rand
+def temperature(Irms): #constant temperature
+    temp = 50 + Irms
     return temp
 
-def vibration(): #constant vibration
-    rand = randint(1,10)
-    vibr = 2 + rand 
+def vibration(res): #constant vibration
+    resConst = 5
+    vibr = randint(1,resConst*res)
     return vibr
 
-def motorEncoder(): 
-    p12 = GPIO.PWM(12, 40) #Blink LED on GPIO 18, pin 12 with f=40Hz
+def motorEncoder():
+    global p12
+    global freq
+    pulses = 40
+    p12 = GPIO.PWM(12, pulses) #Blink LED on GPIO 18, pin 12 with f=40Hz
     p12.start(50) #50% duty cycle
-    return p12
-if __name__ == "__main__":
-    hostip = "10.0.0.12"
+    freq = pulses/10
+    return freq
 
+if __name__ == "__main__":
+    hostip = "192.168.137.185"
     #Board/Port Setup
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(11, GPIO.IN) #GPIO17 Receiving the 5 Vc from the PLC
     GPIO.setup(12, GPIO.OUT) #GPIO18 Pulse Width Modulation
     try:
         print("Running...")
+        motorEncoder()
         while True: #Publish single and multiple messages to these topics
             publish.single("Motor/dcvoltage", DCVoltage(), hostname=hostip)
-            publish.multiple(voltage(freq), hostname=hostip)
-            publish.single("Motor/temperature", temperature(), hostname=hostip)
-            publish.single("Motor/vibration", vibration(), hostname=hostip)
-            publish.multiple(current(freq), hostname=hostip)
+            publish.multiple(voltage(freq, res), hostname=hostip)
+            publish.single("Motor/temperature", temperature(Irms), hostname=hostip)
+            publish.single("Motor/vibration", vibration(res), hostname=hostip)
+            publish.multiple(current(freq, res), hostname=hostip)
             time.sleep(0.1)
     except KeyboardInterrupt:
         print ("Ctrl C - Ending Program")
-        
+
     finally:
+        global p12
+        p12.stop
         GPIO.cleanup()
