812ee356d3b4d31d7154f0d1a279bb3da38c8afe
Daimen Ambers <daimen.ambers@capgemini.com>
2019-02-21 16:05:06 -0500

    Rearranging name, main function

--- mqttSubscribe.py
+++ mqttSubscribe.py
@@ -1,15 +1,16 @@
 #MQTT Client
 import paho.mqtt.client as mqtt
 
-hostip = "192.168.137.247"
+
     
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
     #Subscribing in on_connect() - if we lose the connection and reconnect then subscription will be renewed
-    client.subscribe("Motor/#") #Subscribes to the Voltage, Current, Temp, Resistance and Vibration Messages
-    #client.subscribe("Motor/resistance")
+    #client.subscribe("Motor/#") #Subscribes to the Voltage, Current, Temp, Resistance and Vibration Messages
+    client.subscribe("Motor/resistance")
+    client.subscribe("Motor/chaes")
     
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
@@ -29,6 +30,7 @@ def on_message(client, userdata, msg):
         print("The resistance is 4")
     elif msg.payload == "5" and msg.topic == "Motor/resistance":
         res = 5
+        client.publish("Motor/changeRes", res)
         print("The resistance is 5")
     elif msg.payload == "0" and msg.topic == "Motor/resistance":
         res = 0
@@ -38,14 +40,16 @@ def on_disconnect(client, userdata, flags, rc=0):
     print("Disconnected result code "+str(rc))
             
     
-#Create an MQTT client and attach our routines to it
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-client.on_disconnect = on_disconnect
 
-client.connect(hostip, 1883, 60)
 if __name__ == "__main__":
+    hostip = "192.168.137.247"
+    #Create an MQTT client and attach our routines to it
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.on_disconnect = on_disconnect
+
+    client.connect(hostip, 1883, 60)
     try:
         client.loop_forever()
     except KeyboardInterrupt:
