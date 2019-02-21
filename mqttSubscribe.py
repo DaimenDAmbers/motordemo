8c3d97b67117a6f83ccd9f5859691db1db02759b
Daimen Ambers <daimen.ambers@capgemini.com>
2019-02-21 15:44:51 -0500

    Working with callback function

--- mqttSubscribe.py
+++ mqttSubscribe.py
@@ -1,10 +1,7 @@
 #MQTT Client
-from math import *
-from random import randint
-import time
 import paho.mqtt.client as mqtt
 
-hostip = "192.168.137.185"
+hostip = "192.168.137.247"
     
 
 def on_connect(client, userdata, flags, rc):
@@ -36,17 +33,23 @@ def on_message(client, userdata, msg):
     elif msg.payload == "0" and msg.topic == "Motor/resistance":
         res = 0
         print("The resistance is 0")
+        
+def on_disconnect(client, userdata, flags, rc=0):
+    print("Disconnected result code "+str(rc))
             
     
 #Create an MQTT client and attach our routines to it
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
+client.on_disconnect = on_disconnect
 
 client.connect(hostip, 1883, 60)
-
-try:
-    client.loop_forever()
-except KeyboardInterrupt:
-    print("Ctrl+C Exiting program")
+if __name__ == "__main__":
+    try:
+        client.loop_forever()
+    except KeyboardInterrupt:
+        client.disconnect()
+        client.loop_stop()
+        print("Ctrl+C Exiting program")
     
