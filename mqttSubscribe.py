828c7c7617944e8d0a6b3d8464a2c771cd70b66c
Daimen Ambers <daimen.ambers@capgemini.com>
2019-02-21 16:20:09 -0500

    Able to see updated resistance on dashboard

--- mqttSubscribe.py
+++ mqttSubscribe.py
@@ -2,39 +2,20 @@
 import paho.mqtt.client as mqtt
 
 
-    
-
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
     #Subscribing in on_connect() - if we lose the connection and reconnect then subscription will be renewed
     #client.subscribe("Motor/#") #Subscribes to the Voltage, Current, Temp, Resistance and Vibration Messages
     client.subscribe("Motor/resistance")
-    client.subscribe("Motor/chaes")
+    #client.subscribe("Motor/changeRes")
     
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
-
-
-    if msg.payload == "1" and msg.topic == "Motor/resistance": 
-        res = 1
-        print("The resistance is 1")
-    elif msg.payload == "2" and msg.topic == "Motor/resistance":
-        res = 2
-        print("The resistance is 2")
-    elif msg.payload == "3" and msg.topic == "Motor/resistance":
-        res = 3
-        print("The resistance is 3")
-    elif msg.payload == "4" and msg.topic == "Motor/resistance":
-        res = 4
-        print("The resistance is 4")
-    elif msg.payload == "5" and msg.topic == "Motor/resistance":
-        res = 5
+    
+    if msg.topic == "Motor/resistance":
+        res = msg.payload
         client.publish("Motor/changeRes", res)
-        print("The resistance is 5")
-    elif msg.payload == "0" and msg.topic == "Motor/resistance":
-        res = 0
-        print("The resistance is 0")
         
 def on_disconnect(client, userdata, flags, rc=0):
     print("Disconnected result code "+str(rc))
