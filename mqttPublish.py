5de70249db3e0d3c85e692ff6cb46532d02d5f71
Daimen Ambers <daimen.ambers@capgemini.com>
2019-02-25 16:43:55 -0500

    Discovering the issue with updating the dashboard with new slider values

--- mqttPublish.py
+++ mqttPublish.py
@@ -8,42 +8,40 @@ from datetime import datetime
 
 
 
-def DCVoltage(): #Voltage coming from the PLC
+def inputVoltage(): #This determines if the Rpi is receiving an input from the PLC
     Vc = GPIO.input(11)
     if Vc == 1:
         status = "On"
-        V0 = 5
     else:
         status = "Off"
-        V0 = 0
-    return [status, V0]
+    return status        
 
 
-def current(freq, res): #input the frequency from frequency function
-    resConst = 5
-    V0 = 100
+def current(freq, load, vCtrl): #input the frequency, load and vCtrl from outside sources
+    loadConst = 5
     d = datetime.now()
     uSec = d.microsecond
-    secs = d.second
     mSec = uSec / 1000.0
     global Irms
-    I1 = ((V0/(resConst*res))*sin(2*pi*freq*mSec))
-    Irms = (V0/(resConst*res))/(sqrt(2))
+    I1 = ((vCtrl/(loadConst*load))*sin(2*pi*freq*mSec))
+    I2 = ((vCtrl/(loadConst*load))*sin(2*pi*freq*mSec) + pi*(2/3))
+    I3 = ((vCtrl/(loadConst*load))*sin(2*pi*freq*mSec) + pi*(4/3))
+    Irms = (vCtrl/(loadConst*load))/(sqrt(2))
     return Irms
 
 
 def temperature(Irms): #constant temperature
-    temp = 50 + Irms
+    temp = 20 + ((Irms**3)*0.002)
     return temp
 
-def vibration(res): #constant vibration
-    vibr = randint(1,res)
+def vibration(load, vCtrl): #constant vibration
+    vibr = randint(1,int(0.1*(load + vCtrl)+1))
     return vibr
 
-def motorEncoder():
+def motorEncoder(vCtrl):
     global p12
     global freq
-    pulses = 40
+    pulses = 8 * vCtrl
     p12 = GPIO.PWM(12, pulses) #Blink LED on GPIO 18, pin 12 with f=40Hz
     p12.start(50) #50% duty cycle
     freq = pulses/10
@@ -51,15 +49,15 @@ def motorEncoder():
 
 
 if __name__ == "__main__":
-    #Defining Global Constants and Variables
-    res = 1
-    V0 = 100 #Amplified Voltage for visual purposes
-    I0 = 20 #
-    freq = 0
-    Irms = 0
+#    #Defining Global Constants and Variables
+#    load = 1
+#    vCtrl = 1 #Amplified Voltage for visual purposes
+#    res = 1
+#    freq = 0
 
-    #Get ip address
-    hostip = "10.0.0.8"
+
+    #Get ip addloads
+    hostip = "192.168.137.34"
     
     #Board/Port Setup
     GPIO.setmode(GPIO.BOARD)
@@ -67,21 +65,18 @@ if __name__ == "__main__":
     GPIO.setup(12, GPIO.OUT) #GPIO18 Pulse Width Modulation
     try:
         print("Running...")
-        motorEncoder()
-        
+        motorEncoder(vCtrl)
         while True: #Publish single and multiple messages to these topics
-            status, DCVolts = DCVoltage()
-            publish.single("Motor/dcvoltage", DCVolts, hostname=hostip)
-            publish.single("Motor/status", status, hostname=hostip)
+            status = inputVoltage()
+            publish.single("Motor/status", status, hostname=hostip)            
+            publish.single("Motor/vibration", vibration(load, vCtrl), hostname=hostip)
+            publish.single("Motor/rmsCurrent", current(freq, load, vCtrl), hostname=hostip)
             publish.single("Motor/temperature", temperature(Irms), hostname=hostip)
-            publish.single("Motor/vibration", vibration(res), hostname=hostip)
-            publish.single("Motor/rmsCurrent", current(freq, res), hostname=hostip)
-            print(DCVolts)
-            time.sleep(0.1)
+            time.sleep(1)
     except KeyboardInterrupt:
         print ("Ctrl C - Ending Program")
 
     finally:
         global p12
-        p12.stop
+        p12.stop()
         GPIO.cleanup()
--- mqttSubscribe.py
+++ mqttSubscribe.py
@@ -3,14 +3,13 @@ import paho.mqtt.client as mqtt
 import mqttPublish
 import RPi.GPIO as GPIO
 
-
+vCtrl = 2
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
     #Subscribing in on_connect() - if we lose the connection and reconnect then subscription will be renewed
     client.subscribe("Motor/#") #Subscribes to the Voltage, Current, Temp, Resistance and Vibration Messages
-    global res
-    res = 1
+    client.subscribe("PLC/ctrlvoltage")
     
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
@@ -18,31 +17,38 @@ def on_message(client, userdata, msg):
     if msg.topic == "Motor/dcvoltage":
         client.publish("Motor/status", status)
         print(DCVolts)
+        
+    if msg.topic == "PLC/ctrlvoltage":
+        global vCtrl
+        vCtrl = int(msg.payload)
+#        mqttPublish.motorEncoder(vCtrl)
+#        client.publish("Motor/rmsCurrent", mqttPublish.current(freq, res, vCtrl))
+#        client.publish("Motorl/vibration", mqttPublish.vibration(res, vCtrl))
+#        print(vCtrl)
     
-    if msg.topic == "Motor/resistance":
-        global res
+    if msg.topic == "Motor/resistance": #When the resistnace value changes, the new resistance is published to the functions
         res = int(msg.payload)
-        client.publish("Motor/rmsCurrent", mqttPublish.current(freq, res))
-        client.publish("Motor/vibration", mqttPublish.vibration(res))
-        client.publish("Motor/temperature", mqttPublish.temperature(res))
+        client.publish("Motor/rmsCurrent", mqttPublish.current(freq, res, vCtrl))
+        client.publish("Motor/vibration", mqttPublish.vibration(res, vCtrl))
+        client.publish("Motor/temperature", mqttPublish.temperature(Irms))
         
 #    if msg.topic == "Motor/rmsCurrent":
 #        client.publish("Motor/temperature", mqttPublish.temperature(Irms))
         
 def on_disconnect(client, userdata, flags, rc=0):
     print("Disconnected result code "+str(rc))
+    client.loop_stop()
             
     
 
 if __name__ == "__main__":
-    hostip = "10.0.0.8"
+    hostip = "192.168.137.34"
     #Board/Port Setup
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(11, GPIO.IN) #GPIO17 Receiving the 5 Vc from the PLC
     GPIO.setup(12, GPIO.OUT) #GPIO18 Pulse Width Modulation
-    
-    res = 1
-    freq = mqttPublish.motorEncoder()
+
+    #freq = mqttPublish.motorEncoder(vCtrl)
     
     
     
@@ -55,22 +61,27 @@ if __name__ == "__main__":
     client.connect(hostip, 1883, 60)
     try:
         while True:
-            Irms = mqttPublish.current(freq, res)
-            status, DCVolts = mqttPublish.DCVoltage()
-            vibration = mqttPublish.vibration(res)
-            temperature = mqttPublish.temperature(Irms)
-            
-            client.publish("Motor/dcvoltage", DCVolts)
-            client.publish("Motor/vibration", vibration)
-            client.publish("Motor/temperature", temperature)
-            client.publish("Motor/rmsCurrent", Irms)
-            
-            print("The test resistance is",res)
-            client.loop(.1)
+            client.publish("Motor/ctrlvoltage", vCtrl)
+            client.loop()
+            mqttPublish.time.sleep(1)
+#        while True:
+#            #res = mqttPublish.resistance()
+#            Irms = mqttPublish.current(freq, res, vCtrl)
+#            status = mqttPublish.inputVoltage()
+#            vibration = mqttPublish.vibration(res, vCtrl)
+#            temperature = mqttPublish.temperature(Irms)
+#            
+#            client.publish("Motor/dcvoltage", DCVolts)
+#            client.publish("Motor/vibration", vibration)
+#            client.publish("Motor/temperature", temperature)
+#            client.publish("Motor/rmsCurrent", Irms)
+#            
+#            print("The test resistance is",res)
+#            client.loop()
+#            mqttPublish.time.sleep(1)
 
     except KeyboardInterrupt:
         client.disconnect()
-        client.loop_stop()
         print("Ctrl+C Exiting program")
     finally:
         GPIO.cleanup()
